03d51d346ee84fb23e2f5a28960a60637caef939
maxpipoka <martincardozo@gmail.com>
2021-04-19 09:24:26 -0300

    Terminado el ejercicio 04 clase 03

--- 03_Tercera_clase/ejercicio_04.py
+++ 03_Tercera_clase/ejercicio_04.py
@@ -3,8 +3,30 @@
 # iterarla y mostrar números divisibles por cinco, y si encuentra un 
 # número mayor que 150, detenga la iteración del bucle
 
+import os
+import sys
+
+####################################################################################
 #Funciones
 
+def borrarPantalla(): #Funcion para limpiar pantalla detectando SO
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system ("cls")
 
+def iterarLista(lista1):
+    menorA150 = True
 
+    for i in lista1:
+        if i > 150:
+            break
+        if i%5 == 0:
+            print(f'{i} es divisible por 5.')
+
+####################################################################################
 # Codigo principal
+borrarPantalla()
+lista1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+iterarLista(lista1)
+sys.exit()
